711da473f7f106b4e6ab45b62b6d9b4f722a2731
Sunbochen Tang <20776348+Tangsun@users.noreply.github.com>
2025-08-25 00:13:02 -0400

    latest change, perhaps train c

--- L63/sunbochen_supercloud_train.sh
+++ L63/sunbochen_supercloud_train.sh
@@ -7,18 +7,19 @@
 # 0824_17/22: Last batch of experiments seem to show somehow lr=5e-5 is the sweet spot for training unconstrained models; however, the model predictions don't turn out to be doing well
 
 for lr in 5e-5; do
-  for lam in 10.0 1.0 0.1 0.01 1e-3 1e-4; do
+  for lam in 1.0 0.1 0.01 1e-3 1e-4 1e-5 1e-6; do
     sbatch train_single_sunbochen.sh \
     --epoch 4000 \
-    --c-init 60.0 \
+    --c-init 50.0 \
     --discrete-proj \
     --lam-reg-vol $lam \
-    --lr $lr
+    --lr $lr \
+    --trainable_c
   done
 done
 
-for lr in 5e-5; do
-  sbatch train_single_sunbochen.sh \
-    --epoch 4000 \
-    --lr $lr
-done
+# for lr in 5e-5; do
+#   sbatch train_single_sunbochen.sh \
+#     --epoch 4000 \
+#     --lr $lr
+# done
