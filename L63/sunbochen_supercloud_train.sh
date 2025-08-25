58063a5769fb2ea57ef86b2c668177c79f4d7703
Sunbochen Tang <20776348+Tangsun@users.noreply.github.com>
2025-08-25 11:31:26 -0400

    lower regularization loss

--- L63/sunbochen_supercloud_train.sh
+++ L63/sunbochen_supercloud_train.sh
@@ -7,7 +7,7 @@
 # 0824_17/22: Last batch of experiments seem to show somehow lr=5e-5 is the sweet spot for training unconstrained models; however, the model predictions don't turn out to be doing well
 
 for lr in 5e-5; do
-  for lam in 1e-8 1e-9 1e-10; do
+  for lam in 1e-8 1e-9 1e-10 1e-11; do
     sbatch train_single_sunbochen.sh \
     --epoch 4000 \
     --c-init 50.0 \
